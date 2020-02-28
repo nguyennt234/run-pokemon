6b1248340dbbfee2262bbad08501238179408b7a
Nguyen03 Nguyen Trung <nguyennt03@mn.kbnn.vn>
2020-02-28 13:37:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import {useFetch} from './hook';
+import { useFetch } from './hook';
 import './App.css';
 
 function App() {
-  const [data, loading] = useFetch(
-    "http://localhost:3000/api/user"
-  );
+  const [data, loading] = useFetch("/api/user");
   return (
     <>
       <h1>Photos</h1>
       {loading ? (
         "Loading..."
       ) : (
-        <ul>
-          {data.map(({ _id, ten}) => (
-            <li key={`photo-${_id}`}>
-              {ten}
-            </li>
-          ))}
-        </ul>
-      )}
+          <ul>
+            {data["users"].map(({ _id, ten }) => (
+              <li key={`photo-${_id}`}>
+                {ten}
+              </li>
+            ))}
+          </ul>
+        )}
     </>
   );
 }
